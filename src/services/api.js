0f12b6601d47b3a067f4a0637f50cfec4f18be0b
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-06-21 13:12:31 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,42 +1,28 @@
 const BASE_URL = "https://blogapicluster-6two.onrender.com";
-
-
-// ========================
 // HELPER (SAFE RESPONSE HANDLER)
-// ========================
 const handleResponse = async (response) => {
-  const contentType = response.headers.get("content-type");
-
-  if (!response.ok) {
+const contentType = response.headers.get("content-type");
+if (!response.ok) {
     const text = await response.text();
     throw new Error(text || "Request failed");
   }
-
-  if (!contentType || !contentType.includes("application/json")) {
+if (!contentType || !contentType.includes("application/json")) {
     const text = await response.text();
     throw new Error("Server returned non-JSON response: " + text);
   }
-
-  return response.json();
+return response.json();
 };
-
-
-// ========================
-// POSTS
-// ========================
+// Get POSTS
 export const getPosts = async () => {
   const response = await fetch(`${BASE_URL}/posts`);
   return handleResponse(response);
 };
-
-
+//Create Posts
 export const createPost = async (postData) => {
   const formData = new FormData();
-
   formData.append("title", postData.title);
   formData.append("content", postData.content);
   formData.append("authorId", postData.authorId);
-
 if (postData.image && postData.image instanceof File) {
   formData.append("image", postData.image);
 }
@@ -47,8 +33,7 @@ if (postData.image && postData.image instanceof File) {
 
   return handleResponse(response);
 };
-
-
+//Delete Posts
 export const deletePost = async (id) => {
   const response = await fetch(`${BASE_URL}/posts/${id}`, {
     method: "DELETE",
@@ -57,10 +42,7 @@ export const deletePost = async (id) => {
   return handleResponse(response);
 };
 
-
-// ========================
 // USERS
-// ========================
 export const getUsers = async () => {
   const response = await fetch(`${BASE_URL}/users`);
   return handleResponse(response);
